ddd67a277022e0f9ba049ebc21fa6b2b4007d3fe
ESSI TSONDO Franck Patrick <essitsondo.frankpatrick@esprit.tn>
2021-05-06 15:31:55 +0100

    new

---
+++ views/backend/sideNavBar.php
@@ -0,0 +1,34 @@
+<nav class="side-navbar">
+      <div class="side-navbar-wrapper">
+        <!-- Sidebar Header    -->
+        <div class="sidenav-header d-flex align-items-center justify-content-center">
+          <!-- User Info-->
+          <div class="sidenav-header-inner text-center"><img src="img/logo.jpg" alt="person" width="325" height="260" class="img-fluid rounded-circle">
+            <h2 class="h5">Espritheque</h2><span>Se former Autrement</span>
+          </div>
+          <!-- Small Brand information, appears on minimized sidebar-->
+          <div class="sidenav-header-logo"><a href="index.php" class="brand-small text-center"> <strong>B</strong><strong class="text-primary">D</strong></a></div>
+        </div>
+        <!-- Sidebar Navigation Menus-->
+        <div class="main-menu">
+          <h5 class="sidenav-heading">Main</h5>
+          <ul id="side-main-menu" class="side-menu list-unstyled">                  
+            <li><a href="index.php"> <i class="icon-home"></i>Home</a></li>
+            <li><a href="index.php"> <i class="fas fa-blog"></i>Etudiant</a></li>
+            <li><a href="index.php"> <i class="fas fa-comments"></i>Forum </a></li>
+            <li><a href="index.php"> <i class="fas fa-car"></i>Evènement</a></li>
+            <li><a href="index.php"> <i class="fas fa-shopping-bag"></i>Cours</a></li>
+            <li><a href="e-consultation.php"> <i class="fas fa-user-md"></i>Consulation</a></li>
+            <li><a href="index.php"> <i class="fas fa-comment-dots"></i>Actualités</a></li>
+            <li><a href="#exampledropdownDropdown" aria-expanded="false" data-toggle="collapse"> <i class="fab fa-product-hunt"></i></i>Gestion des Revues </a>
+              <ul id="exampledropdownDropdown" class="collapse list-unstyled ">
+                <li><a href="cathegorieProd.php?page=1">Ouvrage</a></li>
+                <li><a href="animalProd.php?page=1">Revues</a></li> 
+                <li><a href="produits.php?page=1">Produits</a></li>
+              </ul>
+            </li>
+            <li><a href="charts.html"> <i class="fas fa-chart-pie"></i>Statistiques</a></li> 
+          </ul>
+        </div>
+      </div>
+    </nav>
